a7149fb8d422e8b0e9919a96d57f9b7715dd7445
adrianbontea <contact@adrianbontea.com>
2017-01-15 17:31:41 +0200

    Removed project reference to .net framework class library.

--- src/ConsoleAppNetCore/Program.cs
+++ src/ConsoleAppNetCore/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using Newtonsoft.Json;
+using ClassLibraryNetStandard;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var test1 = new ClassLibraryNetStandard.Test { SomeProperty = "One", SomeOtherProperty = "Two" };
-        Console.WriteLine($"Hello from .net core! Here's one .net standard Test instance:{JsonConvert.SerializeObject(test1)}");
-
-        var test2 = new ClassLibraryNetFramework1.Test { SomeProperty = "One", SomeOtherProperty = 2 };
-        Console.WriteLine($"Here's one .net framework Test instance:{JsonConvert.SerializeObject(test2)}");
+        var test = new Test { SomeProperty = "One", SomeOtherProperty = "Two" };
+        Console.WriteLine($"Hello from .net core! Here's one .net standard Test instance:{JsonConvert.SerializeObject(test)}");
 
         Console.ReadLine();
     }
